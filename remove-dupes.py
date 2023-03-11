8934d7bd928ed0ecef64c35a156d346c99d296e0
Alex Lo Storto <lostorto.business@gmail.com>
2023-03-11 15:23:20 +0000

    Updated remove-dupes.py to be dynamic and use regex

--- remove-dupes.py
+++ remove-dupes.py
@@ -1,35 +1,94 @@
 import os
+import re
 
 # Absolute path to the directory you want to search
-ROOT = r"/storage/emulated/0/"
-FILE_LIST = []
-FILE_NAMES = []
-SUFFIX = '.py'
-USE_SUFFIX = False
+ROOT = r"C:\Users\[Users]\[pythonFiles]"
 
 
 def main():
-    assert os.path.isdir(ROOT)
-    for root, _, files in os.walk(ROOT):
-        for file in files:
-            # append the file name to the list if it ends with '.py'
-            path = os.path.join(root, file)
-            if file.endswith(SUFFIX) and USE_SUFFIX:
-                FILE_LIST.append(path)
-            elif not USE_SUFFIX:
-                fileName = path[len(ROOT)+1:]
-                FILE_LIST.append(path)
-                print(fileName)
-                fileNameCut = fileName.replace('(1)', "")
-            if fileNameCut in FILE_NAMES:
-                print("THIS FILE ALREADY EXISTS")
-                os.remove(ROOT+'/'+fileNameCut)
-            print()
-            FILE_NAMES.append(fileName)
+    global ROOT
 
-    # print all the file names
-    # for name in FILE_NAMES:
-    #     print(name)
+    def getInput(array, question):
+        userInput = input(question)
+        print()
+
+        while True:
+            # Check input is acceptable
+            if userInput == '':
+                return False
+            elif not userInput.isdigit() or int(userInput) < 1 or int(userInput) > len(array):
+                print(f"Input has to be a number 1-{len(array)}")
+                userInput = input(question)
+                continue
+            else:
+                # Chooses file
+                userInput = int(userInput)
+                return userInput
+
+    def yesNo(question):
+        userInput = input(question)
+        print()
+
+        while True:
+            # Check input is acceptable
+            if userInput == '':
+                print(f"Don't leave me empty!")
+            elif userInput.lower() in ['y', 'ye', 'yes', 'yeah']:
+                return True
+            elif userInput.lower() in ['n', 'no', 'nop', 'nope']:
+                return False
+
+    def removeDupes(path):
+        assert os.path.isdir(ROOT)
+
+        files = [f for f in os.listdir(ROOT) if os.path.isfile(os.path.join(path, f))]
+
+        dupFiles = []
+        for file in files:
+            position = re.search('\(\d+\).\w+', file)
+            fileNameCut = re.sub('\(\d+\)', '', file)
+            if position is not None and fileNameCut in files:
+                dupFiles.append(file)
+
+        if len(dupFiles) == 0:
+            print("No duplicate files detected\n")
+            input("Press ENTER to exit")
+            return
+
+        print("---FILES---")
+        for file in dupFiles:
+            print(file)
+
+        if yesNo("Delete files (y/n): "):
+            for file in dupFiles:
+                os.remove(os.path.join(ROOT, file))
+            print(f"Deleted {len(dupFiles)} files")
+
+        input("Press ENTER to exit")
+
+    assert os.path.isdir(ROOT)
+
+    while True:
+        dirs = [d for d in os.listdir(ROOT) if os.path.isdir(os.path.join(ROOT, d))]
+
+        if len(dirs) == 0:
+            print("\nRunning script\n")
+            removeDupes(os.path.join(ROOT))
+            break
+
+        # Choose directory
+        for i in range(len(dirs)):
+            print(f"{i+1}) {dirs[i]}")
+
+        dir = getInput(dirs, "Choose directory (press ENTER to choose CWD): ")
+
+        if not dir:
+            print("Running script\n")
+            removeDupes(os.path.join(ROOT))
+            break
+
+        print(f"You chose '{dirs[dir-1]}'\n")
+        ROOT = os.path.join(ROOT, dirs[dir-1])
 
 
 if __name__ == '__main__':
