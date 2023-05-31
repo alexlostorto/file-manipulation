e68be81ab11338ccf3510f730255652d14f5032e
Alex Lo Storto <lostorto.business@gmail.com>
2023-05-31 12:25:17 +0100

    Improve regex for remove-dupes.py

--- remove-dupes.py
+++ remove-dupes.py
@@ -9,37 +9,36 @@ from lib.files import traverse
 ROOT = r"C:\Users\[Users]\[pythonFiles]"
 
 
-def main():
-    global ROOT
+def removeDupes(path):
+    assert os.path.isdir(path)
 
-    def removeDupes(path):
-        assert os.path.isdir(ROOT)
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
-        files = [f for f in os.listdir(ROOT) if os.path.isfile(os.path.join(path, f))]
+    dupFiles = []
+    for file in files:
+        position = re.search('[\w_\ ]+\d+\.\w+', file)
+        fileNameCut = re.sub(r'_*\s*\d+(?=.)', '', file)
+        if position is not None and fileNameCut in files:
+            dupFiles.append(file)
 
-        dupFiles = []
-        for file in files:
-            position = re.search('\(\d+\).\w+', file)
-            fileNameCut = re.sub('\(\d+\)', '', file)
-            if position is not None and fileNameCut in files:
-                dupFiles.append(file)
-
-        if len(dupFiles) == 0:
-            print("No duplicate files detected\n")
-            input("Press ENTER to exit")
-            return
-
-        print("---FILES---")
-        for file in dupFiles:
-            print(file)
-
-        if yesNo("Delete files (y/n): "):
-            for file in dupFiles:
-                os.remove(os.path.join(ROOT, file))
-            print(f"Deleted {len(dupFiles)} files")
-
+    if len(dupFiles) == 0:
+        print("No duplicate files detected\n")
         input("Press ENTER to exit")
+        return
 
+    print("---FILES---")
+    for file in dupFiles:
+        print(file)
+
+    if yesNo("Delete files (y/n): "):
+        for file in dupFiles:
+            os.remove(os.path.join(path, file))
+        print(f"Deleted {len(dupFiles)} files")
+
+    input("Press ENTER to exit")
+
+
+def main():
     assert os.path.isdir(ROOT)
 
     sourceDir = traverse(ROOT, "Choose source directory (press ENTER to choose CWD): ")
